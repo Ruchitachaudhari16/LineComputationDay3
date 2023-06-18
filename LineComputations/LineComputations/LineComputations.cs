b48d265cb84de545d69823e46460368db8022dae
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-06-18 14:49:05 -0400

    [Ruchita] Add: Length of Line1 and Line2

---
+++ LineComputations/LineComputations/LineComputations.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComputations
+{
+    public class LineComputations
+    {
+       int xOne, yOne, xTwo, yTwo;
+       int xThree,yThree,xFour,yFour;
+        public void LengthOfLine()
+        {
+            Console.WriteLine("Enter the Coordinates of Line1");
+            Console.WriteLine("Enter the x1 coordinate");
+            xOne=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the x2 Coordinate");
+            xTwo=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the y1 Coordinate");
+            yOne=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the y2 Coordinate");
+            yTwo=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Enter the Coordinates of Line2");
+            Console.WriteLine("Enter the x3 coordinate");
+            xThree=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the x4 coordinate");
+            xFour=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the y3 coordinate");
+            yThree=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the y4 coordinates");
+            yFour=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("-------------------------------------");
+            //Calculate the length of Line1
+            double LengthOfLineOne = Math.Sqrt((xTwo - xOne)*(xTwo - xOne)+(yTwo - yOne)*(yTwo - yOne));
+            Console.WriteLine("Length of Line1 is:" + LengthOfLineOne);
+            Console.WriteLine("-------------------------------------");
+            //Calculate the length of Line2
+            double LengthOfLineTwo = Math.Sqrt((xFour - xThree) * (xFour - xThree) + (yFour - yThree) + (yFour - xThree));
+            Console.WriteLine("Length of Line2 is:" + LengthOfLineTwo);
+
+
+
+        }
+    }
+}
---
+++ LineComputations/LineComputations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComputations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Computation Problem");
+            LineComputations lineComputations = new LineComputations();
+            lineComputations.LengthOfLine();
+            Console.ReadLine();
+        }
+    }
+}
